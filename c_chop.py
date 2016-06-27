c3f59d28176c3acb1d9966923acfbb653235d9ab
wolfram74 <phaugen@gmail.com>
2016-06-27 16:26:45 -0500

    sudocode for c and d solutions

--- c_chop.py
+++ c_chop.py
@@ -1,2 +1,15 @@
+'''
+alternative approach
+dissassemble the subject.
+construct less and greater arrays.
+if the middle component of subject is bigger than query, move left half into less
+if the middle component of subject is smaller than query, move the right half into greater
+if the middle component equals the query, return middle index + length of less
+
+'''
 def chop(query, subject):
     return -1
+
+'''
+
+'''
--- d_chop.py
+++ d_chop.py
@@ -1,2 +1,7 @@
+'''
+informed chop:
+inspect the index that would be the result of a roughly even spacing of values based on the first and last value.
+'''
+
 def chop(query, subject):
     return -1
