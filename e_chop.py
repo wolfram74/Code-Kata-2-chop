c873e8f646f6af43e81a42dade523e611d23e6c2
wolfram74 <phaugen@gmail.com>
2016-06-28 15:21:55 -0500

    finished

--- e_chop.py
+++ e_chop.py
@@ -6,15 +6,13 @@ def chop(query, subject):
     if len(subject) == 0:
         return -1
     if subject[0] > query or subject[-1] < query:
-        print(query, subject, subject[0], subject[-0], 'NERPS')
         return -1
     if subject[0] == query:
         return 0
     difference = subject[-1] - subject[0]
-    length = len(subject)
-    check = int(length*(query-subject[0])/difference)
+    max_index = len(subject)-1
+    check = int(max_index*(query-subject[0])/difference)
     test = subject[check]
-    print(check, subject)
     if test == query:
         return check
     elif test > query:
@@ -32,4 +30,5 @@ errors:
     first draft compiles and looking left isn't really working, it seems.
     apparently negative indexing worked differently than I expected.
     infinite recursion.
+    fixed infinite recursion and out of indexing problem.
 '''
