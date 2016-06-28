ad6ff608172696ebd374787c2ef1672e2e195404
wolfram74 <phaugen@gmail.com>
2016-06-27 19:06:08 -0500

    first trial and some curious errors

--- c_chop.py
+++ c_chop.py
@@ -8,8 +8,30 @@ if the middle component equals the query, return middle index + length of less
 
 '''
 def chop(query, subject):
+    less = []
+    greater = []
+    candidates = subject
+    while candidates:
+        print(candidates, less, greater)
+        check = len(candidates)//2
+        value = candidates[check]
+        if value == query:
+            return check + len(less)
+        if value < query:
+            less += candidates[:check]
+            candidates = candidates[check:]
+        if value > query:
+            greater = candidates[check:] + greater
+            candidates = candidates[:check]
+        if len(candidates) == 1:
+            return -1
     return -1
 
 '''
-
+errors:
+    runs on the first try and passes surprising number of tests.
+    fails to find first element in collection
+salamanders
+angelo + erin: refresh basics, strings vs symbols vs variable names
+david + bahman: want more references
 '''
